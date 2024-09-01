0e3a769abab4c3cc52e8037c780181df7221d34d
paulojunior1308 <pauloesjr2@gmail.com>
2024-09-01 17:26:35 -0300

    Verbos GET, POST, PUT e DELETE

---
+++ 07_Verbs-GET-POST-PUT-DELETE/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/exceptions/UnsupportedMathOperationException.java
@@ -0,0 +1,14 @@
+package br.com.erudio.exceptions;
+
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class UnsupportedMathOperationException extends RuntimeException {
+
+  public UnsupportedMathOperationException(String ex) {
+    super(ex);
+  }
+
+  private static final long serialVersionUID = 1L;
+}
