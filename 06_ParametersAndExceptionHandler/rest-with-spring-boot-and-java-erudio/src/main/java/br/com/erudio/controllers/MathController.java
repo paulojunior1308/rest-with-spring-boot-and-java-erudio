ffe21300db71ec562538fe60ce07cd90996baaa2
paulojunior1308 <pauloesjr2@gmail.com>
2024-09-01 13:39:30 -0300

    Path Params e Tratamento de Exceptions

---
+++ 06_ParametersAndExceptionHandler/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/MathController.java
@@ -0,0 +1,81 @@
+package br.com.erudio.controllers;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.erudio.converters.NumberConverter;
+import br.com.erudio.exceptions.UnsupportedMathOperationException;
+import br.com.erudio.math.SimpleMath;
+
+@RestController
+public class MathController {
+
+	private SimpleMath math = new SimpleMath();
+	
+	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+	public Double sum (
+		@PathVariable(value = "numberOne") String numberOne, 
+		@PathVariable(value = "numberTwo") String numberTwo ) throws Exception 
+	{
+		if(!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)){
+			throw new UnsupportedMathOperationException("Please set a numeric value!");
+		} 
+		return math.sum(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
+	}
+
+	@RequestMapping(value = "/subtraction/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+	public Double subtraction (
+		@PathVariable(value = "numberOne") String numberOne, 
+		@PathVariable(value = "numberTwo") String numberTwo ) throws Exception 
+	{
+		if(!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)){
+			throw new UnsupportedMathOperationException("Please set a numeric value!");
+		} 
+		return math.subtraction(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
+	}
+
+	@RequestMapping(value = "/multiplication/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+	public Double multiplication (
+		@PathVariable(value = "numberOne") String numberOne, 
+		@PathVariable(value = "numberTwo") String numberTwo ) throws Exception 
+	{
+		if(!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)){
+			throw new UnsupportedMathOperationException("Please set a numeric value!");
+		} 
+		return math.multiplication(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
+	}
+
+	@RequestMapping(value = "/division/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+	public Double division (
+		@PathVariable(value = "numberOne") String numberOne, 
+		@PathVariable(value = "numberTwo") String numberTwo ) throws Exception 
+	{
+		if(!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)){
+			throw new UnsupportedMathOperationException("Please set a numeric value!");
+		} 
+		return math.division(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
+	}
+
+	@RequestMapping(value = "/media/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+	public Double media (
+		@PathVariable(value = "numberOne") String numberOne, 
+		@PathVariable(value = "numberTwo") String numberTwo ) throws Exception 
+	{
+		if(!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)){
+			throw new UnsupportedMathOperationException("Please set a numeric value!");
+		} 
+		return math.media(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
+	}
+
+	@RequestMapping(value = "/raiz/{numberOne}", method = RequestMethod.GET)
+	public Double raiz (
+		@PathVariable(value = "numberOne") String number) throws Exception 
+	{
+		if(!NumberConverter.isNumeric(number)){
+			throw new UnsupportedMathOperationException("Please set a numeric value!");
+		} 
+		return math.raiz(NumberConverter.convertToDouble(number));
+	}
+}
---
+++ 06_ParametersAndExceptionHandler/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/math/SimpleMath.java
@@ -0,0 +1,35 @@
+package br.com.erudio.math;
+
+public class SimpleMath {
+
+  	public Double sum (Double numberOne, Double numberTwo) 
+	{
+
+		return numberOne + numberTwo;
+	}
+
+	public Double subtraction (Double numberOne, Double numberTwo)
+	{
+		return numberOne - numberTwo;
+	}
+
+	public Double multiplication (Double numberOne, Double numberTwo) 
+	{
+		return numberOne * numberTwo;
+	}
+
+	public Double division (Double numberOne, Double numberTwo)
+	{
+		return numberOne / numberTwo;
+	}
+
+	public Double media (Double numberOne, Double numberTwo) 
+	{
+		return (numberOne + numberTwo)/2;
+	} 
+
+public Double raiz (Double number)
+	{
+		return Math.sqrt(number);
+	}
+}
