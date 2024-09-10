32836e8e5071a090c3c313ad95bf42de58bb8a18
paulojunior1308 <pauloesjr2@gmail.com>
2024-09-10 17:07:27 -0300

    Adicionando Suporte a Migration com Flyway

---
+++ 10_MigrationsFlyway/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -0,0 +1,70 @@
+package br.com.erudio.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.erudio.data.vo.v1.PersonVO;
+import br.com.erudio.data.vo.v2.PersonVOV2;
+import br.com.erudio.services.PersonServices;
+
+@RestController
+@RequestMapping("/api/person/v1")
+public class PersonController {
+	
+	@Autowired
+	private PersonServices service;
+	//private PersonVOServices service = new PersonVOServices();
+	
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<PersonVO> findAll ()
+	{
+		return service.findAll();
+	}
+	
+	@GetMapping(value = "/{id}",
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public PersonVO findById (@PathVariable(value = "id") Long id)  
+	{
+		return service.findById(id);
+	}
+	
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, 
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public PersonVO create (@RequestBody PersonVO PersonVO) 
+	{
+		return service.create(PersonVO);
+	}
+
+	@PostMapping(value = "/v2",consumes = MediaType.APPLICATION_JSON_VALUE, 
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public PersonVOV2 createV2 (@RequestBody PersonVOV2 PersonVO) 
+	{
+		return service.createV2(PersonVO);
+	}
+
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, 
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public PersonVO update (@RequestBody PersonVO PersonVO) 
+	{
+		return service.update(PersonVO);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<?> delete (@PathVariable(value = "id") Long id)  
+	{
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+}
---
+++ 10_MigrationsFlyway/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,9 @@
+INSERT INTO `person` (id, address, first_name, gender, last_name) VALUES 
+(1,'São Paulo','Paulo','Male','Junior'),
+(2,'Minas Gerais','Paulo','Male','Jr'),
+(3,'Minas Gerais','Eduardo','Male','Souza'),
+(4,'São Paulo','Eduardo','Male','Souza Junior'),
+(5,'São Paulo','Paulo','Male','Souza Junior'),
+(6,'São Paulo','Paulo','Male','Junior Souza'),
+(7,'São Paulo','Paulo','Male','Eduardo Junior Souza'),
+(8,'São Paulo','Paulo Junior','Male','Eduardo Junior Souza');
