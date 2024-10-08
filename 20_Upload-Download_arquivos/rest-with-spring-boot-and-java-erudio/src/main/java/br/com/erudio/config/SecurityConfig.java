023c9f8d853059622292011f0815d0672d402021
paulojunior1308 <pauloesjr2@gmail.com>
2024-10-08 17:34:21 -0300

    Atualizando

--- 20_Upload-Download_arquivos/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/SecurityConfig.java
+++ 20_Upload-Download_arquivos/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer.JwtConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
